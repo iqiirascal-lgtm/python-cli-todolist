3a635072e3448aff4896dd87c69fa2d5609129a9
iqiirascal-lgtm <iqiirascal@gmail.com>
2026-05-07 16:50:10 +0700

    feat: implement add_task input and dictionary storage

--- main.py
+++ main.py
@@ -17,7 +17,21 @@ def show_tasks():
 
 def add_task():
     """Fitur 2: Menambahkan task baru (Reezqee)"""
-    pass
+    print("\n--- ➕ TAMBAH TUGAS BARU ---")
+    title = input("Masukkan judul tugas: ").strip()
+    assignee = input("Masukkan nama assignee (PIC): ").strip()
+    
+    if title == "" or assignee == "":
+        print("❌ Gagal: Judul dan Assignee tidak boleh kosong!")
+    else:
+        # Menambahkan data ke list tasks dalam bentuk dictionary
+        new_task = {
+            "title": title,
+            "assignee": assignee,
+            "status": "pending"  # Status default saat baru ditambah
+        }
+        tasks.append(new_task)
+        print(f"✅ Tugas '{title}' berhasil ditambahkan untuk {assignee}.")
 
 def update_status():
     """Fitur 3: Mengubah status task (Reezqee)"""
@@ -42,7 +56,7 @@ def main():
         choice = input("Pilih menu (1-6): ").strip()
         if choice == "1":
             show_tasks()
-        elif choice == "6":
+        elif choice == "2":
             break
 
 if __name__ == "__main__":
